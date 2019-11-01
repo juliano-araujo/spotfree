23e461a8ef1c15306eedb85834f7388f762e9994
Juliano <julianosilva82010@gmail.com>
2019-11-01 00:20:24 -0400

    Configuração Inicial da interface do Firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+import config from '../config/firebase'
+
+firebase.initializeApp(config)
+
+export const firestore = firebase.firestore();
+export const strorage = firebase.storage();
+export const auth = firebase.auth();
