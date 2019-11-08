ecbd30c03862e332b8d86d55f5394c86216b4faf
Juliano <julianosilva82010@gmail.com>
2019-11-08 01:46:16 -0400

    Roteamento do login

---
+++ src/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Route, Redirect } from 'react-router-dom';
+
+import {isAuthenticated } from 'services/firebase';
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+	return (
+		<Route {...rest}
+		render={props => (
+			isAuthenticated() ? (
+			<Component {...props} />
+			) : (
+				<Redirect to={{ pathname: "/login", state: { from: props.location }}} />
+			)
+		)}/>
+	);
+}
