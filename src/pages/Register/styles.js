a0cad829d76768104070cfb8496206a16dc3b139
Juliano <julianosilva82010@gmail.com>
2019-11-27 02:55:39 -0400

    Corrigindo erro no backgroud do cadastro

--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
+import background from 'assets/images/background.jpg';
+
 export const Container = styled.div`
 	display: table;
 	width: 100%;
 	height: 100%;
-	background: url('background.jpg') no-repeat bottom center scroll;
+	background: url(${background}) no-repeat bottom center scroll;
 	background-position: bottom;
 	background-position: 30% 45%;
 	background-size: cover;
